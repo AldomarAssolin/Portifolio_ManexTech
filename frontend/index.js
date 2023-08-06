34bdcf8ca446142a191b5ab4fa2cc9c74c9c5a7a
Aldomar Assolin <assolinaldomar@gmail.com>
2023-08-05 23:02:08 -0300

    adicionado Página Projetos

---
+++ frontend/index.js
@@ -0,0 +1,52 @@
+import express from 'express'
+import ejs from 'ejs';
+import projects from './public/js/projects.cjs';
+import cursos from './public/js/cursos.cjs';
+import * as fs from 'fs'; // Importe o módulo fs
+const app = express();
+
+app.set('view engine', 'ejs'); // Define o mecanismo de visualização como EJS
+app.use(express.static('public')); // Define a pasta para arquivos estáticos (CSS, imagens, etc.)
+
+app.get('/', (req, res) => {
+
+    try {
+        // Importar o conteúdo do package.json como uma string
+        const packageJsonContent = fs.readFileSync('./package.json', 'utf-8');
+
+        // Converter o conteúdo do JSON para um objeto
+        const packageJson = JSON.parse(packageJsonContent);
+    
+        const authorName = packageJson.author;
+    
+        res.render('index', {projects, cursos, authorName}); // Renderiza o arquivo EJS chamado "index.ejs" na pasta "views"
+      } catch (error) {
+        console.error('Erro:', error);
+        res.status(500).send('Erro interno do servidor');
+      }
+    
+    
+});
+
+<<<<<<< HEAD
+app.get('/projects', (req, res) => {
+
+  try {
+  
+      res.render('projects', {projects}); // Renderiza o arquivo EJS chamado "index.ejs" na pasta "views"
+    
+    } catch (error) {
+      console.error('Erro:', error);
+      res.status(500).send('Erro interno do servidor');
+    }
+  
+  
+});
+
+app.listen(3003, () => {
+    console.log('Servidor iniciado na porta 3003');
+=======
+app.listen(3003, () => {
+    console.log('Servidor iniciado na porta 3000');
+>>>>>>> 83c469b425318aaabc374fb36de2751319f7575e
+});
