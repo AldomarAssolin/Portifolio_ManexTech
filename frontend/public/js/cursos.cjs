83c469b425318aaabc374fb36de2751319f7575e
Aldomar Assolin <assolinaldomar@gmail.com>
2023-08-05 22:27:16 -0300

    Implementação do EJS
    Implementado EJS e node.

---
+++ frontend/public/js/cursos.cjs
@@ -0,0 +1,18 @@
+module.exports = [
+    {
+        image: '',
+        name: 'Análise e Desenvolvimento de Sistemas.',
+        local: 'Uniasselvi',
+        type: 'Graduação',
+        initial_date: '09/09/2023',
+        end_date: '25/07/2023'
+    },
+    {
+        image: '',
+        name: 'Técnico em Soldagem.',
+        local: 'CTISM',
+        type: 'Técnico',
+        initial_date: '05/03/2018',
+        end_date: '12/12/2019'
+    }
+]
