d79eff69f731ed4ca279036386644be15f12f71e
Aldomar Assolin <assolinaldomar@gmail.com>
2023-08-05 22:27:16 -0300

    Implementação do EJS
    Implementado EJS e node.

---
+++ frontend/public/js/projects.cjs
@@ -0,0 +1,36 @@
+
+module.exports = [
+    {
+        name: "Portifólio Soldagem",
+        objetive:"Estudos",
+        description: "Portifólio simples, apenas frontend, foi prototipado em Figma, usado git e github.",
+        qualification: "Frontend",
+        technologies: ["React", "HTML", "CSS"],
+        initial_date: "01/08/2023",
+        end_date: "25/09/2023",
+        link_github: "https://github.com/AldomarAssolin/portifolio-solda",
+        link_project: "https://aldomarassolin.vercel.app/"
+    },
+    {
+        name: "Portifolio Dev",
+        objetive: "Estudos",
+        description: "Portifólio simples, apenas frontend, foi prototipado em Figma, usado git e github.",
+        qualification: "fullstack",
+        technologies: ["React", "HTML", "CSS", "nodejs", "MySQL"],
+        initial_date: "25/09/2023",
+        end_date: "10/12/2023",
+        link_github: "https://github.com/AldomarAssolin/Portifolio_ManexTech",
+        link_project: "https://www.manexstore.com.br"
+    },
+    {
+        name: "Site Revenda Carro",
+        objetive: "Projeto",
+        description: "Site para revenda de automóveis, integração com banco de dados e gataways diversos.",
+        qualification: "Fullstack",
+        technologies: ["React", "HTML", "CSS", "nodejs", "MySQL"],
+        initial_date: "02/05/2023",
+        end_date: "09/07/2023",
+        link_github: "https://github.com/AldomarAssolin/revenda-de-carros",
+        link_project:  "https://manexstore.online"
+    }
+]
---
+++ frontend/public/js/scripts.js
@@ -0,0 +1,6 @@
+// import {author} from './package.json'
+
+// const authorName = author//"<%= require('./package.json').author %>";
+
+//     // Inserir o nome do autor no HTML
+//     document.querySelector(".nameAutor").textContent = authorName;
